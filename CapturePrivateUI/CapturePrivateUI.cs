3b106fe585298ef5244776221420eadeabec62f5
art0007i <art0007i@gmail.com>
2023-06-14 02:39:38 +0200

    add extra config options

--- CapturePrivateUI/CapturePrivateUI.cs
+++ CapturePrivateUI/CapturePrivateUI.cs
@@ -13,17 +13,34 @@ namespace CapturePrivateUI
     {
         public override string Name => "CapturePrivateUI";
         public override string Author => "art0007i";
-        public override string Version => "1.0.0";
+        public override string Version => "1.1.0";
         public override string Link => "https://github.com/art0007i/CapturePrivateUI/";
 
         [AutoRegisterConfigKey]
         static ModConfigurationKey<bool> ENABLE_KEY = new("enable", "When enabled, finger photo will render private UI.", () => true);
+        [AutoRegisterConfigKey]
+        static ModConfigurationKey<bool> TAG_FILTER_ENABLE_KEY = new("tag_filter_enable", "When enabled, all cameras on a slot with the specified tag will render private UI.", () => false);
+        [AutoRegisterConfigKey]
+        static ModConfigurationKey<string> TAG_FILTER_KEY = new("tag_filter", "The tag that cameras will require to render private UI. Only works when the above option is enabled.", () => "RenderPrivateUI");
         public static ModConfiguration config;
 
         public override void OnEngineInit()
         {
             config = GetConfiguration();
 
+            config.OnThisConfigurationChanged += (e) => {
+                if(e.Key == TAG_FILTER_ENABLE_KEY || e.Key == TAG_FILTER_KEY)
+                {
+                    // I love updating every thing
+                    Engine.Current.WorldManager.Worlds.Do((w) =>
+                    {
+                        w.RunSynchronously(() =>
+                        {
+                            w.RootSlot.GetComponentsInChildren<Camera>().Do((c) => c.MarkChangeDirty());
+                        });
+                    });
+                }
+            };
             Harmony harmony = new Harmony("me.art0007i.CapturePrivateUI");
             harmony.PatchAll();
         }
@@ -46,6 +63,17 @@ namespace CapturePrivateUI
                 {
                     camField.Target.RenderPrivateUI = config.GetValue(ENABLE_KEY);
                 }
+            }
+        }
+
+        [HarmonyPatch(typeof(Camera), nameof(Camera.RenderPrivateUI), MethodType.Getter)]
+        class TagFilterPatch
+        {
+            public static bool Prefix(Camera __instance, ref bool __result)
+            {
+                if (!config.GetValue(TAG_FILTER_ENABLE_KEY)) return true;
+                __result = __instance.Slot.Tag == config.GetValue(TAG_FILTER_KEY);
+                return false;
             }
         }
     }
